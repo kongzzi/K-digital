cf4b851a48b812d2b44ad15b9e6e892c301bbef4
Munju Jeong <mayfour@gmail.com>
2025-08-05 23:00:05 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js 파일
+
+// 페이지 로딩이 완료되면 실행될 코드
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("페이지가 성공적으로 로드되었습니다. script.js 파일이 연결되었습니다.");
+
+    // 나중에 동적인 기능을 이곳에 추가할 수 있습니다.
+});
